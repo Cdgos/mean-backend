4113d0d84bcc3ff259f47347aadb1cec5a372063
Sebastian Casadiegos <sebastianc.dev@gmail.com>
2022-10-24 00:04:48 -0500

    commit

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(express.json());
 
 app.use('/api/productos', require('./routes/producto'));
 
-app.listen(4000, () =>{
+app.listen(process.env.PORT || 3000, () =>{
     console.log('El servidor está corriendo perfectamente')
 })
